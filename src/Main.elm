f9419a26fc967ee1998f9b739e144d4a63b81ed2
Jon Moon <jmoon@cisco.com>
2020-12-09 13:50:56 +0000

    Day 1

--- src/Main.elm
+++ src/Main.elm
@@ -1,30 +1,92 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, div, text)
+import Html exposing (Html, b, button, div, text)
+import Html.Events exposing (onClick)
+import Http
+import Json.Decode as Decode exposing (Decoder)
+import RemoteData exposing (RemoteData(..), WebData)
 
 
 type alias Model =
-    {}
+    { input : WebData String }
+
+
+loadInput : Cmd Msg
+loadInput =
+    Http.get
+        { url = "day1_input.txt"
+        , expect =
+            Http.expectString (RemoteData.fromResult >> InputLoaded)
+        }
+
+
+calculate_ : Int -> Int -> List Int -> List Int -> Maybe Int
+calculate_ target layers candidates others =
+    if layers == 0 then
+        if List.sum candidates == target then
+            Just (List.product candidates)
+
+        else
+            Nothing
+
+    else
+        case others of
+            [] ->
+                Nothing
+
+            x :: xs ->
+                case calculate_ target (layers - 1) (x :: candidates) xs of
+                    Just r ->
+                        Just r
+
+                    Nothing ->
+                        calculate_ target layers candidates xs
+
+
+calculate : Int -> Int -> String -> Maybe Int
+calculate target layers input =
+    String.split "\n" input
+        |> List.map String.toInt
+        |> List.filterMap identity
+        |> calculate_ target layers []
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { input = NotAsked }, Cmd.none )
 
 
 type Msg
-    = NoOp
+    = InputLoaded (WebData String)
+    | Run
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        InputLoaded inputData ->
+            ( { model | input = inputData }, Cmd.none )
+
+        Run ->
+            ( { model | input = Loading }, loadInput )
 
 
 view : Model -> Html Msg
 view model =
-    div [] [ text "Hello, World!" ]
+    div []
+        [ button [ onClick Run ] [ text "Run" ]
+        , text
+            (Debug.toString
+                (case model.input of
+                    Success input ->
+                        Just ( calculate 2020 2 input, calculate 2020 3 input )
+
+                    _ ->
+                        Nothing
+                )
+            )
+        ]
 
 
 main : Program () Model Msg
